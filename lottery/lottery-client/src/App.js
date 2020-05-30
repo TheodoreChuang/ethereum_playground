ef5577b4342416c3748db5d81f3629549d243bb9
Theodore Chuang <chuangtheodore@gmail.com>
2020-05-31 00:06:46 +1000

    feat(lottery-client): Basic client: display data, enter lottery, pick winner

--- lottery/lottery-client/src/App.js
+++ lottery/lottery-client/src/App.js
@@ -3,20 +3,69 @@ import web3 from './web3'
 import lottery from './LotteryContract'
 
 function App() {
-  const [manager, setManager] = useState('')
+  const [lotteryState, setLotteryState] = useState({ manager: '', players: [], lotteryBalance: '' })
+  const [enterValue, setEnterValue] = useState('')
+  const [message, setMessage] = useState('')
 
   const loadBlockchainData = async () => {
     const manager = await lottery.methods.manager().call()
-    setManager(manager)
+    const players = await lottery.methods.getPlayers().call()
+    const lotteryBalance = await web3.eth.getBalance(lottery.options.address)
+    setLotteryState({ manager, players, lotteryBalance })
   }
 
   useEffect(() => {
     loadBlockchainData()
   }, [])
 
-  web3.eth.getAccounts().then(console.log)
+  const handleEnterLottery = async (evt) => {
+    evt.preventDefault()
+    const accounts = await web3.eth.getAccounts()
 
-  return <div className="App">Lottery. Managed by: {manager}</div>
+    setMessage('Processing your transaction...')
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(enterValue, 'ether'),
+    })
+    setEnterValue('')
+    setMessage('You have been entered into the lottery. Good Luck!')
+    loadBlockchainData()
+  }
+
+  const handlePickWinner = async () => {
+    const accounts = await web3.eth.getAccounts()
+
+    setMessage('Processing your transaction...')
+    await lottery.methods.pickWinner().send({ from: accounts[0] })
+    setMessage('A winner has been picked.')
+    loadBlockchainData()
+  }
+
+  return (
+    <div className="App">
+      <h1>Lottery Contract</h1>
+      <div>This contract is managed by: {lotteryState.manager}</div>
+      <div>There are currently {lotteryState.players.length} players in the pool.</div>
+      <div>Current pot is: {web3.utils.fromWei(lotteryState.lotteryBalance, 'ether')} ETH</div>
+
+      <hr />
+      <form onSubmit={handleEnterLottery}>
+        <h4>Want to try your luck?</h4>
+        <div>
+          <label>Amount to ETH to enter </label>
+          <input value={enterValue} onChange={(evt) => setEnterValue(evt.target.value)} />
+          <button>Enter</button>
+        </div>
+      </form>
+
+      <hr />
+      <h4>Ready to pick a winner?</h4>
+      <button onClick={handlePickWinner}>Pick a winner</button>
+
+      <hr />
+      <div>{message}</div>
+    </div>
+  )
 }
 
 export default App
