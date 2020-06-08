10b17d6d4bd461d6401e99958017dcd31aca3e4a
Theodore Chuang <chuangtheodore@gmail.com>
2020-06-08 22:52:42 +1000

    feat(crowdfunding-client): add campaign new page

--- crowdfunding/pages/campaigns/new.js
+++ crowdfunding/pages/campaigns/new.js
@@ -1,18 +1,48 @@
-import React from 'react'
-import { Form, Button } from 'semantic-ui-react'
+import React, { useState } from 'react'
+import { Form, Button, Input, Message } from 'semantic-ui-react'
+
+import web3 from '../../ethereum/web3'
+import factory from '../../ethereum/factory'
 import Layout from '../../components/Layout'
 
-const CampaignNew = () => (
-  <Layout>
-    <h3>Create a Campaign</h3>
-    <Form>
-      <Form.Field>
-        <label>Minimum Contribution</label>
-        <input />
-      </Form.Field>
-      <Button primary>Create</Button>
-    </Form>
-  </Layout>
-)
+const CampaignNew = () => {
+  const [minimumContribution, setMinimumContribution] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    setErrorMessage('')
+    setLoading(true)
+    try {
+      const accounts = await web3.eth.getAccounts()
+      await factory.methods.createCampaign(minimumContribution).send({ from: accounts[0] })
+    } catch (err) {
+      setErrorMessage(err.message)
+    }
+    setLoading(false)
+  }
+
+  return (
+    <Layout>
+      <h3>Create a Campaign</h3>
+      <Form onSubmit={handleSubmit} error={!!errorMessage}>
+        <Form.Field>
+          <label>Minimum Contribution</label>
+          <Input
+            value={minimumContribution}
+            onChange={(event) => setMinimumContribution(event.target.value)}
+            labelPosition="right"
+            label="wei"
+          />
+        </Form.Field>
+        <Message error header="Oops" content={errorMessage} />
+        <Button primary loading={loading}>
+          Create
+        </Button>
+      </Form>
+    </Layout>
+  )
+}
 
 export default CampaignNew
