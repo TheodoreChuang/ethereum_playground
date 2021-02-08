88ace60884bc1458f6d07489b7f3796d83723649
TheodoreChuang <chuangtheodore@gmail.com>
2021-02-08 23:31:54 +1100

    feat(dapp30-d5): Add basic dApp frontend

--- dapp-30/day5-crud/frontend/client/index.js
+++ dapp-30/day5-crud/frontend/client/index.js
@@ -1,52 +1,121 @@
-import Web3 from 'web3';
-import Crud from '../build/contracts/Crud.json';
+import Web3 from "web3";
+import Crud from "../build/contracts/Crud.json";
 
 let web3;
 let crud;
 
 const initWeb3 = () => {
   return new Promise((resolve, reject) => {
-    if(typeof window.ethereum !== 'undefined') {
+    if (typeof window.ethereum !== "undefined") {
       const web3 = new Web3(window.ethereum);
-      window.ethereum.enable()
+      window.ethereum
+        .enable()
         .then(() => {
-          resolve(
-            new Web3(window.ethereum)
-          );
+          resolve(new Web3(window.ethereum));
         })
-        .catch(e => {
+        .catch((e) => {
           reject(e);
         });
       return;
     }
-    if(typeof window.web3 !== 'undefined') {
-      return resolve(
-        new Web3(window.web3.currentProvider)
-      );
+    if (typeof window.web3 !== "undefined") {
+      return resolve(new Web3(window.web3.currentProvider));
     }
-    resolve(new Web3('http://localhost:9545'));
+    resolve(new Web3("http://localhost:9545"));
   });
 };
 
 const initContract = () => {
   const deploymentKey = Object.keys(Crud.networks)[0];
-  return new web3.eth.Contract(
-    Crud.abi, 
-    Crud
-      .networks[deploymentKey]
-      .address
-  );
+  return new web3.eth.Contract(Crud.abi, Crud.networks[deploymentKey].address);
 };
 
 const initApp = () => {
+  let accounts = [];
+  web3.eth.getAccounts().then((_accounts) => {
+    accounts = _accounts;
+  });
+
+  const $create = document.getElementById("create");
+  const $createResult = document.getElementById("create-result");
+
+  $create.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const name = e.target.elements[0].value;
+
+    crud.methods
+      .userCreate(name)
+      .send({ from: accounts[0] })
+      .then(() => {
+        $createResult.innerHTML = `New user ${name} was successfully created`;
+      })
+      .catch((err) => {
+        $createResult.innerHTML = `Error occurred while creating new user: ${name}. ${err.message}`;
+      });
+  });
+
+  const $read = document.getElementById("read");
+  const $readResult = document.getElementById("read-result");
+
+  $read.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const id = e.target.elements[0].value;
+
+    crud.methods
+      .userRead(id)
+      .call()
+      .then((result) => {
+        $readResult.innerHTML = `ID: ${result[0]} | Name: ${result[1]}`;
+      })
+      .catch((err) => {
+        $readResult.innerHTML = `Error occurred while getting details for user ID ${id}. ${err.message}`;
+      });
+  });
+
+  const $edit = document.getElementById("edit");
+  const $editResult = document.getElementById("edit-result");
+
+  $edit.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const id = e.target.elements[0].value;
+    const name = e.target.elements[1].value;
+
+    crud.methods
+      .userUpdate(id, name)
+      .send({ from: accounts[0] })
+      .then(() => {
+        $editResult.innerHTML = `Changed name of user ID ${id} to ${name}`;
+      })
+      .catch((err) => {
+        $editResult.innerHTML = `Error occurred while editing user ID ${id}'s name. ${err.message}`;
+      });
+  });
+
+  const $delete = document.getElementById("delete");
+  const $deleteResult = document.getElementById("delete-result");
+
+  $delete.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const id = e.target.elements[0].value;
+
+    crud.methods
+      .userDestroy(id)
+      .send({ from: accounts[0] })
+      .then(() => {
+        $deleteResult.innerHTML = `Deleted user ID ${id}`;
+      })
+      .catch((err) => {
+        $deleteResult.innerHTML = `Error occurred while deleting user ID ${id}. ${err.message}`;
+      });
+  });
 };
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   initWeb3()
-    .then(_web3 => {
+    .then((_web3) => {
       web3 = _web3;
       crud = initContract();
-      initApp(); 
+      initApp();
     })
-    .catch(e => console.log(e.message));
+    .catch((err) => console.log(err.message));
 });
--- dapp-30/day5-crud/frontend/contracts/Crud.sol
+++ dapp-30/day5-crud/frontend/contracts/Crud.sol
@@ -1,40 +1,51 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 contract Crud {
-  struct User {
-    uint id;
-    string name;
-  }
-  User[] public users;
-  uint public nextId = 1;
-
-  function create(string memory name) public {
-    users.push(User(nextId, name));
-    nextId++;
-  }
-
-  function read(uint id) view public returns(uint, string memory) {
-    uint i = find(id);
-    return(users[i].id, users[i].name);
-  }
-
-  function update(uint id, string memory name) public {
-    uint i = find(id);
-    users[i].name = name;
-  }
-
-  function destroy(uint id) public {
-    uint i = find(id);
-    delete users[i];
-  }
-
-  function find(uint id) view internal returns(uint) {
-    for(uint i = 0; i < users.length; i++) {
-      if(users[i].id == id) {
-        return i;
-      }
+    struct User {
+        uint256 id;
+        string name;
     }
-    revert('User does not exist!');
-  }
+    User[] public users;
+    /// @dev start nextId to avoid false positive with 0
+    uint256 public nextId = 1;
 
+    /// @notice Create a new user
+    function userCreate(string memory name) public {
+        users.push(User(nextId, name));
+        nextId++;
+    }
+
+    /// @notice Get a user's details
+    function userRead(uint256 id)
+        public
+        view
+        returns (uint256 _id, string memory _name)
+    {
+        uint256 index = findIndex(id);
+        return (users[index].id, users[index].name);
+    }
+
+    /// @notice Update an existing user
+    function userUpdate(uint256 id, string memory name) public {
+        uint256 index = findIndex(id);
+        users[index].name = name;
+    }
+
+    /// @notice Destroy an existing user
+    /// @dev id filled with default values, id are not rerranged
+    function userDestroy(uint256 id) public {
+        uint256 index = findIndex(id);
+        delete users[index];
+    }
+
+    /// @notice Util: Find an user index by its id
+    function findIndex(uint256 id) internal view returns (uint256 index) {
+        for (uint256 i = 0; i < users.length; i++) {
+            if (users[i].id == id) {
+                return i;
+            }
+        }
+        revert("User does not exist");
+    }
 }
