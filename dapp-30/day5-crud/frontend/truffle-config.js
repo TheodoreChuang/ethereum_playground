b93ba4ae2b98275f272734e85ac07a49a441f0a7
TheodoreChuang <chuangtheodore@gmail.com>
2021-02-10 08:11:27 +1100

    feat(dapp30-d5): Add truffle config for ropsten migration.

--- dapp-30/day5-crud/frontend/truffle-config.js
+++ dapp-30/day5-crud/frontend/truffle-config.js
@@ -1,6 +1,20 @@
+const fs = require("fs");
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+const secrets = JSON.parse(fs.readFileSync(".secrets").toString().trim());
+
 module.exports = {
-  networks: {},
-  mocha: {},
+  networks: {
+    // deployed @ https://ropsten.etherscan.io/address/0x2131e38bc0f47f6490d7e6fb70c519cc4c785baf
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider(
+          secrets.seed,
+          `https://ropsten.infura.io/v3/${secrets.projectId}`
+        ),
+      network_id: 3,
+    },
+  },
   compilers: {
     solc: {
       version: "0.8.0",
