4260fdb854eb018bdb809269bfb887c8776fb5b5
teddy <chuangtheodore@gmail.com>
2021-10-27 22:04:42 +1100

    feat(epic-nfts): Update NFT contract, data entirely onchain.

--- buildspace/epic-nfts/README.md
+++ buildspace/epic-nfts/README.md
@@ -1,13 +1,13 @@
 # Epic NFTs
 
 Contracts
-V0.0.0 - NFT with static metadata
+V0.0.0 - NFT with static externally hosted metadata
 
 - https://rinkeby.etherscan.io/address/0xFde44547c97e7D2C7663D711AfE61803ab564280
 - https://testnets.opensea.io/collection/squarenft-iydizzwcc7
 - https://rinkeby.rarible.com/token/0xFde44547c97e7D2C7663D711AfE61803ab564280:0
 
-Static NFT metadata:
+Externally hosted metadata:
 
 https://jsonkeeper.com/b/6479
 
@@ -19,7 +19,49 @@ https://jsonkeeper.com/b/6479
 }
 ```
 
-### Basic Sample Hardhat Project
+V0.0.1 - NFT with on-chain metadata
+
+- https://rinkeby.etherscan.io/address/0xC150E98B2F1e5159F3a9b4E8F2771DF0d1214323
+- https://testnets.opensea.io/collection/squarenft-ibalhqqhbv
+- https://rinkeby.rarible.com/token/0xC150E98B2F1e5159F3a9b4E8F2771DF0d1214323:<tokenId>
+
+Metadata will be stored as base64 encoded JSON.
+
+```svg
+<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">
+    <style>.base { fill: white; font-family: serif; font-size: 14px; }</style>
+    <rect width="100%" height="100%" fill="black" />
+    <text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">EpicLordHamburger</text>
+</svg>
+```
+
+base64 encoded svg:
+
+```
+data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHByZXNlcnZlQXNwZWN0UmF0aW89InhNaW5ZTWluIG1lZXQiIHZpZXdCb3g9IjAgMCAzNTAgMzUwIj4KICAgIDxzdHlsZT4uYmFzZSB7IGZpbGw6IHdoaXRlOyBmb250LWZhbWlseTogc2VyaWY7IGZvbnQtc2l6ZTogMTRweDsgfTwvc3R5bGU+CiAgICA8cmVjdCB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiBmaWxsPSJibGFjayIgLz4KICAgIDx0ZXh0IHg9IjUwJSIgeT0iNTAlIiBjbGFzcz0iYmFzZSIgZG9taW5hbnQtYmFzZWxpbmU9Im1pZGRsZSIgdGV4dC1hbmNob3I9Im1pZGRsZSI+RXBpY0xvcmRIYW1idXJnZXI8L3RleHQ+Cjwvc3ZnPg==
+```
+
+```json
+{
+  "name": "EpicLordHamburger",
+  "description": "An NFT from the highly acclaimed square collection",
+  "image": "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHByZXNlcnZlQXNwZWN0UmF0aW89InhNaW5ZTWluIG1lZXQiIHZpZXdCb3g9IjAgMCAzNTAgMzUwIj4NCiAgICA8c3R5bGU+LmJhc2UgeyBmaWxsOiB3aGl0ZTsgZm9udC1mYW1pbHk6IHNlcmlmOyBmb250LXNpemU6IDE0cHg7IH08L3N0eWxlPg0KICAgIDxyZWN0IHdpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIGZpbGw9ImJsYWNrIiAvPg0KICAgIDx0ZXh0IHg9IjUwJSIgeT0iNTAlIiBjbGFzcz0iYmFzZSIgZG9taW5hbnQtYmFzZWxpbmU9Im1pZGRsZSIgdGV4dC1hbmNob3I9Im1pZGRsZSI+RXBpY0xvcmRIYW1idXJnZXI8L3RleHQ+DQo8L3N2Zz4="
+}
+```
+
+base64 encoded JSON:
+
+```
+data:application/json;base64,ewogICAgIm5hbWUiOiAiRXBpY0xvcmRIYW1idXJnZXIiLAogICAgImRlc2NyaXB0aW9uIjogIkFuIE5GVCBmcm9tIHRoZSBoaWdobHkgYWNjbGFpbWVkIHNxdWFyZSBjb2xsZWN0aW9uIiwKICAgICJpbWFnZSI6ICJkYXRhOmltYWdlL3N2Zyt4bWw7YmFzZTY0LFBITjJaeUI0Yld4dWN6MGlhSFIwY0RvdkwzZDNkeTUzTXk1dmNtY3ZNakF3TUM5emRtY2lJSEJ5WlhObGNuWmxRWE53WldOMFVtRjBhVzg5SW5oTmFXNVpUV2x1SUcxbFpYUWlJSFpwWlhkQ2IzZzlJakFnTUNBek5UQWdNelV3SWo0TkNpQWdJQ0E4YzNSNWJHVStMbUpoYzJVZ2V5Qm1hV3hzT2lCM2FHbDBaVHNnWm05dWRDMW1ZVzFwYkhrNklITmxjbWxtT3lCbWIyNTBMWE5wZW1VNklERTBjSGc3SUgwOEwzTjBlV3hsUGcwS0lDQWdJRHh5WldOMElIZHBaSFJvUFNJeE1EQWxJaUJvWldsbmFIUTlJakV3TUNVaUlHWnBiR3c5SW1Kc1lXTnJJaUF2UGcwS0lDQWdJRHgwWlhoMElIZzlJalV3SlNJZ2VUMGlOVEFsSWlCamJHRnpjejBpWW1GelpTSWdaRzl0YVc1aGJuUXRZbUZ6Wld4cGJtVTlJbTFwWkdSc1pTSWdkR1Y0ZEMxaGJtTm9iM0k5SW0xcFpHUnNaU0krUlhCcFkweHZjbVJJWVcxaWRYSm5aWEk4TDNSbGVIUStEUW84TDNOMlp6ND0iCn0=
+```
+
+### Utils
+
+- https://jsonkeeper.com
+- https://www.svgviewer.dev/
+- https://www.utilities-online.info/base64
+
+## Basic Sample Hardhat Project
 
 This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, a sample script that deploys that contract, and an example of a task implementation, which simply lists the available accounts.
 
--- buildspace/epic-nfts/contracts/EpicNFT.sol
+++ buildspace/epic-nfts/contracts/EpicNFT.sol
@@ -3,14 +3,74 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "hardhat/console.sol";
+
+import {Base64} from "./libraries/Base64.sol";
 
 contract EpicNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
+    // SVG template
+    string openSvg =
+        "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
+    string closeSvg = "</text></svg>";
+
+    // personalities traits
+    string[] firstWords = [
+        "Active",
+        "Affectionate",
+        "Curious",
+        "Easygoing",
+        "Inquisitive",
+        "Intelligent",
+        "Interactive",
+        "Friendly"
+        "Gentle"
+        "Loyal",
+        "Playful",
+        "Sensitive",
+        "Sweet",
+        "Social"
+    ];
+
+    // physical traits
+    string[] secondWords = [
+        "Athletic",
+        "Buff",
+        "Chubby",
+        "Lean"
+        "Patchy",
+        "Plump",
+        "Slender",
+        "Spotted",
+        "Striped",
+        "Ripped"
+    ];
+
+    // cat breeds
+    string[] thirdWords = [
+        "Ragdoll",
+        "ExoticShorthair",
+        "BritishShorthair",
+        "Persian",
+        "MaineCoon",
+        "AmericanShorthair",
+        "DevonRex",
+        "Sphynx",
+        "Scottish Fold",
+        "Abyssinian",
+        "Oriental",
+        "Siamese",
+        "Bengal",
+        "RussianBlue",
+        "Siberian"
+    ];
+
     constructor() ERC721("SquareNFT", "SQUARE") {
-        console.log("This is my NFT contract. Whoa!");
+        console.log("EpicNFT constructor");
     }
 
     /*
@@ -19,15 +79,79 @@ contract EpicNFT is ERC721URIStorage {
     function makeAnEpicNft() public {
         uint256 newItemId = _tokenIds.current();
 
+        string memory first = pickRandomWordFromList(
+            "FIRST_WORD",
+            firstWords,
+            newItemId
+        );
+        string memory second = pickRandomWordFromList(
+            "SECOND_WORD",
+            secondWords,
+            newItemId
+        );
+        string memory third = pickRandomWordFromList(
+            "THIRD_WORD",
+            thirdWords,
+            newItemId
+        );
+        string memory combinedWord = string(
+            abi.encodePacked(first, second, third)
+        );
+
+        string memory finalSvg = string(
+            abi.encodePacked(openSvg, combinedWord, closeSvg)
+        );
+
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        // We set the title of our NFT as the generated word.
+                        '{"name": "',
+                        combinedWord,
+                        '", "description": "A highly acclaimed collection of squares.", "image": "data:image/svg+xml;base64,',
+                        // We add data:image/svg+xml;base64 and then append our base64 encode our svg.
+                        Base64.encode(bytes(finalSvg)),
+                        '"}'
+                    )
+                )
+            )
+        );
+
+        string memory finalTokenUri = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+
+        console.log("\n--------------------");
+        console.log(finalTokenUri);
+        console.log("--------------------\n");
+
         _safeMint(msg.sender, newItemId);
 
-        _setTokenURI(newItemId, "https://jsonkeeper.com/b/6479");
+        _setTokenURI(newItemId, finalTokenUri);
+
+        _tokenIds.increment();
+
         console.log(
             "An NFT w/ ID %s has been minted to %s",
             newItemId,
             msg.sender
         );
+    }
 
-        _tokenIds.increment();
+    function pickRandomWordFromList(
+        string memory salt,
+        string[] memory list,
+        uint256 tokenId
+    ) internal pure returns (string memory) {
+        uint256 rand = random(
+            string(abi.encodePacked(salt, Strings.toString(tokenId)))
+        );
+        rand = rand % list.length;
+        return list[rand];
+    }
+
+    function random(string memory input) internal pure returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(input)));
     }
 }
