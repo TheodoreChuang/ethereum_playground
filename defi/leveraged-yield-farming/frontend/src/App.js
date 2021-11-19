9920e7c0019d9b56ce9e245f59b57e7da154e5a3
teddy <chuangtheodore@gmail.com>
2021-11-20 00:14:57 +1100

    feat(leverage-yield-farm): Add impersonate and snatch DAI for testing.

--- defi/leveraged-yield-farming/frontend/src/App.js
+++ defi/leveraged-yield-farming/frontend/src/App.js
@@ -1,7 +1,7 @@
-import { useState, useEffect } from 'react';
-import getBlockchain from './ethereum.js';
-import addresses from './addresses.js';
-import { ethers } from 'ethers';
+import { useState, useEffect } from "react";
+import getBlockchain from "./ethereum.js";
+import addresses from "./addresses.js";
+import { ethers } from "ethers";
 
 function App() {
   const [yieldFarmer, setYieldFarmer] = useState(undefined);
@@ -17,87 +17,72 @@ function App() {
   }, []);
 
   const openPosition = async e => {
+    console.log("🚀 ~ file: App.js ~ line 20 ~ App ~ e", e);
     e.preventDefault();
     const amountProvided = ethers.utils.parseUnits(e.target.elements[0].value, 18);
+    console.log("🚀 ~ file: App.js ~ line 23 ~ App ~ amountProvided", amountProvided);
     const amountBorrowed = ethers.utils.parseUnits(e.target.elements[1].value, 18);
-    const tx1 = await dai.approve(yieldFarmer.address, amountProvided.add('2'));
+    console.log("🚀 ~ file: App.js ~ line 25 ~ App ~ amountBorrowed", amountBorrowed);
+    const tx1 = await dai.approve(yieldFarmer.address, amountProvided.add("2"));
+    console.log("🚀 ~ openPosition ~ tx1", tx1);
     await tx1.wait();
     const tx2 = await yieldFarmer.openPosition(
-      addresses.solo, 
-      addresses.dai, 
+      addresses.solo,
+      addresses.dai,
       addresses.cDai,
-      amountProvided, 
+      amountProvided,
       amountBorrowed
     );
+    console.log("🚀 ~ openPosition ~ tx2", tx2);
     await tx2.wait();
   };
 
   const closePosition = async e => {
     e.preventDefault();
-    const tx1 = await dai.approve(yieldFarmer.address, '2');
+    const tx1 = await dai.approve(yieldFarmer.address, "2");
+    console.log("🚀 ~ file: App.js ~ line 43 ~ App ~ tx1", tx1);
     await tx1.wait();
-    const tx2 = await yieldFarmer.closePosition(
-      addresses.solo, 
-      addresses.dai, 
-      addresses.cDai
-    );
+    const tx2 = await yieldFarmer.closePosition(addresses.solo, addresses.dai, addresses.cDai);
+    console.log("🚀 ~ file: App.js ~ line 46 ~ App ~ tx2", tx2);
     await tx2.wait();
   };
 
-  if(
-    typeof yieldFarmer === 'undefined'
-  ) {
-    return 'Loading...';
+  if (typeof yieldFarmer === "undefined") {
+    return "Loading...";
   }
 
   return (
-    <div className='container'>
-
-      <div className='row'>
-        <div className='col-sm-12'>
-          <h1 className='text-center'>Yield Farmer</h1>
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-12">
+          <h1 className="text-center">Yield Farmer</h1>
           <div className="jumbotron">
             <h1 className="display-4 text-center">Yield Farm COMP tokens on Compound with leverage</h1>
           </div>
         </div>
       </div>
 
-      <div className='row'>
-        <div className='col-sm-6'>
+      <div className="row">
+        <div className="col-sm-6">
           <h5>Open Position</h5>
           <form className="form-inline" onSubmit={e => openPosition(e)}>
-            <input 
-              type="text" 
-              className="form-control mb-2 mr-sm-2" 
-              placeholder="Amount provided (DAI)"
-            />
-            <input 
-              type="text" 
-              className="form-control mb-2 mr-sm-2" 
-              placeholder="Amount borrowed (DAI)"
-            />
-            <button 
-              type="submit" 
-              className="btn btn-primary mb-2"
-            >
+            <input type="text" className="form-control mb-2 mr-sm-2" placeholder="Amount provided (DAI)" />
+            <input type="text" className="form-control mb-2 mr-sm-2" placeholder="Amount borrowed (DAI)" />
+            <button type="submit" className="btn btn-primary mb-2">
               Submit
             </button>
           </form>
         </div>
 
-        <div className='col-sm-6'>
+        <div className="col-sm-6">
           <h5>Close Position</h5>
           <form className="form-inline" onSubmit={e => closePosition(e)}>
-            <button 
-              type="submit" 
-              className="btn btn-primary mb-2"
-            >
+            <button type="submit" className="btn btn-primary mb-2">
               Submit
             </button>
           </form>
         </div>
       </div>
-
     </div>
   );
 }
--- defi/leveraged-yield-farming/frontend/src/ethereum.js
+++ defi/leveraged-yield-farming/frontend/src/ethereum.js
@@ -1,6 +1,14 @@
 import { ethers, Contract } from "ethers";
+import { JsonRpcProvider } from "@ethersproject/providers";
+import { formatEther, parseEther } from "@ethersproject/units";
+
+import { DAI_ABI } from "./addresses";
 import YieldFarmer from "./contracts/YieldFarmer.json";
 import addresses from "./addresses.js";
+
+// 🏠 Your local provider is usually pointed at your local blockchain
+const localProviderUrl = "http://localhost:8545";
+const localProvider = new JsonRpcProvider(localProviderUrl);
 
 const getBlockchain = () =>
   new Promise((resolve, reject) => {
@@ -11,21 +19,31 @@ const getBlockchain = () =>
         const signer = provider.getSigner();
         const signerAddress = await signer.getAddress();
 
-        console.log(
-          "🚀 ~ file: ethereum.js ~ line 16 ~ window.addEventListener ~ window.ethereum.networkVersion",
-          window.ethereum.networkVersion
-        );
-        console.log(
-          "🚀 ~ file: ethereum.js ~ line 20 ~ window.addEventListener ~ YieldFarmer.networks[window.ethereum.networkVersion].address,",
-          YieldFarmer.networks[window.ethereum.networkVersion].address
-        );
+        // ****** start snatch DAI ******
+        // let accountToImpersonate = "0x1e3d6eab4bcf24bcd04721caa11c478a2e59852d";
+        // await localProvider.send("hardhat_impersonateAccount", [accountToImpersonate]);
+        // const signerImpersonate = await localProvider.getSigner(accountToImpersonate);
+        // console.log(signerImpersonate.provider.getSigner());
+        // const myDaiContract = new Contract(addresses.dai, DAI_ABI, signerImpersonate);
+
+        // const myAddress = await signerImpersonate.getAddress();
+        // console.log("myAddress", myAddress);
+        // console.log("🚀 ~ file: ethereum.js ~ line 35 ~ window.addEventListener ~ myDaiContract", myDaiContract);
+        // const signerDaiBal = await myDaiContract.balanceOf(myAddress);
+        // console.log("vsignerDaiBal", signerDaiBal);
+
+        // let transferbal = parseFloat(formatEther(signerDaiBal)) - 0.01;
+        // myDaiContract.transfer("0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266", parseEther(transferbal.toString()));
+        // ****** end snatch DAI ******
+
         const yieldFarmer = new Contract(
-          YieldFarmer.networks[window.ethereum.networkVersion].address,
+          // YieldFarmer.networks[window.ethereum.networkVersion].address,
+          "0x4EE6eCAD1c2Dae9f525404De8555724e3c35d07B",
           YieldFarmer.abi,
           signer
         );
 
-        const dai = new Contract(addresses.dai, ["function approve(address spender, uint amount) external"], signer);
+        const dai = new Contract(addresses.dai, DAI_ABI, signer);
 
         resolve({ signerAddress, yieldFarmer, dai });
       }
