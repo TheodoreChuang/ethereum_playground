e78e2de5fe6f96b192cb71f4cdc1ba10ea785ac4
teddy <chuangtheodore@gmail.com>
2022-10-20 22:25:36 +1100

    feat(fcc-sol): Add L4 v1.1.0

--- fcc-full-blockchain-solidity-course-js/L4/README.md
+++ fcc-full-blockchain-solidity-course-js/L4/README.md
@@ -3,3 +3,14 @@
 #### v1.0.0
 
 - Goerli at 0x6cA2d9691c1FbA04d2dF14e07b8B277188E68A5f
+- Add FundMe.sol
+  - basic fund()
+  - convert ETH to USD with chainlink price feed
+
+#### v1.1.0
+
+- Goerli at
+- FundMe.sol
+  - store funders
+- Add PriceConverter.sol
+- Refactor pricing and conversion logic to PriceConverter library
--- fcc-full-blockchain-solidity-course-js/L4/contracts/FundMe.sol
+++ fcc-full-blockchain-solidity-course-js/L4/contracts/FundMe.sol
@@ -1,45 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.8;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
 
-/// @notice
+/// @notice Crowd funding
 /// @author teddy
-/// @custom:version 1.0.0
+/// @custom:version 1.1.0
 contract FundMe {
+    // Attach library to type
+    using PriceConverter for uint256;
+
     uint256 public minimumUsd = 49 * 1e18; // match decimals of ETH
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
 
     /// @notice Donors can contribute funds (ETH)
     function fund() public payable {
         require(
-            getConversionRate(msg.value) > minimumUsd,
+            // msg.value is implicitly passed as the first arg into convertEthToUsd
+            msg.value.convertEthToUsd() > minimumUsd,
             "Didn't meet minimal funding amount"
         );
-    }
-
-    /// @notice Get latest ETH price in terms of USD from oracle
-    function getPrice() public view returns (uint256) {
-        // Goerli | Chainlink price feed | ETH / USD - 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(
-            0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
-        );
-        (, int256 price, , , ) = priceFeed.latestRoundData();
-
-        // price has 8 decimals, so multiple by 1e10 to match the decimals of ETH
-        return uint256(price * 1e10);
-    }
-
-    /// @notice ETH in terms of USD
-    function getConversionRate(uint256 ethAmount)
-        public
-        view
-        returns (uint256)
-    {
-        uint256 ethPrice = getPrice();
-
-        // General rule is to multiple/add before divide because Solidity doesn't support floats
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
+        // Optimisation: dedupe multiple contributions from the same sender
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
     }
 
     /// @notice Owner can withdraw all funds into their address
